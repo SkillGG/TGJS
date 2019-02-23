c49509513b4ae8ae794f89858d899a2c2b239126
SGG <skilletssgames@gmail.com>
2019-02-23 02:30:37 +0100

    Coder example setup and first base functions.
    Coder:
    	+Examples
    	+processLine(declaration)
    Lang:
    	+getElement(decl.)
    	+loadDictionary(decl.)
    	+loadDictionaries(done)

--- Coder/coder.js
+++ Coder/coder.js
@@ -1,2 +1,20 @@
 
 let Coder = {};
+
+Coder.parseLine = (element, callback)=>{
+
+	try{
+		let el = element;
+
+		if(!element)
+			throw "Element has been not specified!";
+		if(!callback || typeof callback !== "function")
+			throw "Callback not specified or not a function!";
+
+		// TODO: Line parsing
+
+		return el;
+	}catch(e){
+		return e;
+	}
+};
--- Language/language.js
+++ Language/language.js
@@ -1,2 +1,39 @@
 
 let Language = {};
+
+Language.getElement = (variable, lang)=>{
+
+	
+
+};
+
+Language.loadDictionary = (path, lang)=>{
+
+};
+
+Language.loadDictionaries = (...dictionaries)=>{
+	let dics = Array.from(dictionaries);
+	dics.forEach((e, i)=>{
+		if(!e || !e.path || !e.lang){
+			console.warn(`Dictionary number ${i+1} has not been specified properly.`);
+			return;
+		}
+		this.loadDictionary(e.path, e.lang);
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let Lang = Language;
